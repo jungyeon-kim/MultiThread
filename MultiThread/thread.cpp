3259b9bb97fb4c0a9da79add5baedd4bdf4689fd
Kim Jung Yeon <zoemfhs123@gmail.com>
2020-09-10 21:04:55 +0900

    멀티스레드 테스트케이스 수정
    solution2()에 스레드 개수별 수행시간을 기록하고
    release모드의 최적화를 배제시키기위해 volatiile사용

--- MultiThread/thread.cpp
+++ MultiThread/thread.cpp
@@ -32,7 +32,9 @@ using namespace std::chrono;
 /*
 	주의점)
 	DataRace:	같은 메모리를 여러 스레드에서 읽고 써서 의도치 않는 결과가 나오는 것.
-				(반드시 하나 이상의 쓰기 동작이 있어야함. 그래야 문제가 발생하기에)
+				ex) 레지스터에 변수를 읽어와 연산 후 변수에 저장하려는 도중에 다른 스레드에서 끼어들때
+				※ 반드시 하나 이상의 쓰기 동작이 있어야함. 그래야 문제가 발생하니까.
+				※ 싱글코어에서도 문맥전환이 일어나면 데이터레이스가 일어날 수 있음
 	DeadLock:	무한정으로 대기해 교착상태에 빠지는 것
 				ex) 중복 lock, 둘 이상의 스레드가 서로 사용중인 자원을 요구하면서 각자의 자원을 놓지않을 때
 
@@ -56,11 +58,12 @@ using namespace std::chrono;
 	joinable():	해당 스레드를 join()으로 기다릴 수 있는 상태인지 bool값을 반환한다. (detach된 상태라면 false 반환)
 
 	mutex)
-	lock():		어떤 자원을 다른 스레드에서 사용중이면 대기. 사용중이 아니면 자신의 스레드에서 사용중 표시 (성능저하)
+	lock():		어떤 자원을 다른 스레드에서 사용중이면 대기. 사용중이 아니면 자신의 스레드에서 사용중 표시
 	unLock():	사용중 표시를 지움
 	lock_guard<T>:	lock을 걸고 자신이 포함된 스코프를 빠져나갈때 자동으로 unlock
 
 	※ mutex 객체는 전역변수여야함 (같은 자원을 사용하는 스레드마다 mutex도 같은 객체여야하기 때문)
+	※ 정확성은 올라가지만, 병렬성이 떨어지고 os를 호출하기 때문에 성능저하 발생
 
 	atomic)
 	atomic<T>:							스레드 세이프한 자료형 (mutex보다 빠르지만, 타입제한이 존재)
@@ -72,6 +75,8 @@ using namespace std::chrono;
 	※ Lock 없이 CAS를 이용한 논블로킹 자료구조를 사용해 동기화하는 것이 바람직하다.
 */
 
+constexpr int MAX_THREADS = 64;
+
 // join()과 detach()에 대한 실습코드
 void printID(int myID)
 {
@@ -80,66 +85,70 @@ void printID(int myID)
 
 void solution1()
 {
-	vector<thread> v{};
+	vector<thread> threads{};
 
-	for (int i = 0; i < 10; ++i) v.emplace_back(printID, i);
-	for (auto& thread : v) thread.join();
+	for (int i = 0; i < 10; ++i) threads.emplace_back(printID, i);
+	for (auto& thread : threads) thread.join();
 
-	v.clear();
+	threads.clear();
 	cout << "---------------------------------" << endl;
 
 	for (int i = 0; i < 10; ++i)
 	{
-		v.emplace_back(printID, i);
-		v[i].detach();
+		threads.emplace_back(printID, i);
+		threads[i].detach();
 	}
-	for (auto& thread : v)
+	for (auto& thread : threads)
 		if (thread.joinable())	// join()을 호출할 수 있는지 확인
 			thread.join();		// detach되면 join()이 호출되지 않기 때문에 스레드 실행도중 프로그램이 종료된다.
 }
 
 // 상호배제, 임계영역에 대한 실습코드
-atomic<int> atomic_sum{};	// 스레드 공유자원
 int sum{};					// 스레드 공유자원
+atomic<int> atomicSum{};	// 스레드 공유자원
 mutex m{};					// mutex 객체
 
-void add()
+void add(int numOfThread)
 {
-	int localSum{};
+	volatile int localSum{};	// release 테스트를 위해 volatile 사용
 
-	for (int i = 0; i < 1000000; ++i)
-	{
-		++localSum;		// 부하를 유발하는 lock을 사용하지않고 연산하기 위해 로컬변수를 사용
-		++atomic_sum;	// atomic 객체를 사용하면 로컬변수가 필요없음
-	}
+	// 부하를 유발하는 lock을 사용하지않고 연산하기 위해 로컬변수를 사용
+	for (int i = 0; i < 500000000 / numOfThread; ++i) localSum += 2;
 
 	m.lock();			// lock을 걸어주지않으면 DataRace에 의해 sum은 예상과 다른 값이 됨
 	sum += localSum;	// 로컬변수의 결과를 전역변수(공유자원)에 더해줄때만 lock을 사용
 	m.unlock();
+
+	atomicSum += localSum;	// atomic 객체를 사용하면 lock이 필요없음
 }
 
 void solution2()
 {
-	vector<thread> v{};
+	vector<thread> threads{};
 
-	for (int i = 0; i < 10; ++i) v.emplace_back(add);
-	for (auto& thread : v) thread.join();
+	for (int i = 1; i <= MAX_THREADS; i *= 2)
+	{
+		sum = 0;
+		atomicSum = 0;
+		threads.clear();
+		auto start{ high_resolution_clock::now() };
 
-	cout << "sum: " << sum << endl;
-	cout << "atomic_sum: " << atomic_sum << endl;
+		for (int j = 0; j < i; ++j) threads.emplace_back(add, i);
+		for (auto& thread : threads) thread.join();
+
+		auto duration{ high_resolution_clock::now() - start };
+		cout << i << " Threads" << " Sum = " << sum << " AtomicSum = " << atomicSum;
+		cout << " Duration = " << duration_cast<milliseconds>(duration).count() << " milliseconds\n";
+	}
 }
 
 // 스레드의 이해를 돕기위한 실습코드
-constexpr int MAX_THREADS = 64;
-
 volatile int global{};
 
 void workerThread(int numOfThread)
 {
-	// 문제 1: 데이터 레이스가 발생해 예상밖의 결과가 나옴
-	// 문제 2: 데이터 영역에서 연산하므로 스택 영역에서 연산하는 것 보다 느림
-	// solution2()에서 두 문제를 해결.
-	for (int i = 0; i < 50000000 / numOfThread; ++i) global += 2;
+	// 문제: 데이터 레이스가 발생해 예상밖의 결과가 나옴 -> solution2()에서 해결
+	for (int i = 0; i < 500000000 / numOfThread; ++i) global += 2;
 }
 
 void testThread()
@@ -152,8 +161,8 @@ void testThread()
 		threads.clear();
 		auto start{ high_resolution_clock::now() };
 
-		for (int j = 0; j < i; ++j) threads.emplace_back(thread{ workerThread, i });
-		for (auto& tmp : threads) tmp.join();
+		for (int j = 0; j < i; ++j) threads.emplace_back(workerThread, i);
+		for (auto& thread : threads) thread.join();
 
 		auto duration{ high_resolution_clock::now() - start };
 		cout << i << " Threads" << " Sum = " << global;
@@ -163,7 +172,7 @@ void testThread()
 
 int main()
 {
+	//testThread();
 	//solution1();
 	solution2();
-	//testThread();
 }
